3d19f6afc795f956178d6b953e4debb128aa4361
Dawid Piechota <dpiechotaa@gmail.com>
2022-09-04 23:54:35 +0200

    fix: navbar closed position

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -57,13 +57,13 @@ const Navbar: FC = () => {
           className={
             isOpen
               ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
-              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+              : 'fixed left-[-200%] top-0 p-10 ease-in duration-500'
           }
         >
           <div>
             <div className="flex w-full items-center justify-between">
               <Image
-                src="/../public/assets/navLogo.png"
+                src="http://placekitten.com/352/353"
                 alt="/"
                 width="90"
                 height="70"
